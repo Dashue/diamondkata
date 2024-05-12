a167e7c283315e158ccd07745d468bf6197ac738
Johan Nilsson <johan@softyltd.co.uk>
2024-05-12 18:44:35 +0100

    Diamond is symmetrical

--- Diamond.tests/ImplementationTests.cs
+++ Diamond.tests/ImplementationTests.cs
@@ -38,5 +38,40 @@ namespace Diamond.tests
             Assert.Equal(centerColumnIndex-1, diamond[1].IndexOf("B"));
             Assert.Equal(centerColumnIndex-2, diamond[2].IndexOf("C"));
         }
+
+        [Fact]
+        public void Rows_are_horizontally_symmetrical()
+        {
+            var diamond = Diamond.Create('C');
+            var numberOfRows = diamond.Length;
+            var elementsPriorAndAfter = (numberOfRows -1) / 2;
+            var elementsTotake = elementsPriorAndAfter + 1;
+            
+            var firstHalf = diamond.Take(elementsTotake);
+            var secondHalf = diamond.Skip(elementsPriorAndAfter).Take(elementsTotake);
+
+            Assert.Equal(firstHalf, secondHalf.Reverse());
+        }
+
+        [Fact]
+        public void Rows_are_vertically_symmetrical()
+        {
+            var diamond = Diamond.Create('C');
+            var numberOfRows = diamond.Length;
+            var rowLength = numberOfRows;
+
+            Assert.All(diamond, row =>
+            {
+                var indexOfMiddleCharacter = (numberOfRows - 1) / 2;
+                var numberOfCharactersToTake = indexOfMiddleCharacter + 1;
+
+                var leftPart = row.Substring(0, numberOfCharactersToTake);
+                var rightPart = row.Substring(indexOfMiddleCharacter, numberOfCharactersToTake);
+                
+                Assert.Equal(numberOfCharactersToTake, leftPart.Length);
+                Assert.Equal(numberOfCharactersToTake, rightPart.Length);
+                Assert.Equal(leftPart, new String(rightPart.Reverse().ToArray()));
+            });
+        }
     }
 }
