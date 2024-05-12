5c75a07eba2548e91468aaa3b7e540d5436910b2
Johan Nilsson <johan@softyltd.co.uk>
2024-05-12 19:23:18 +0100

    Diamond renders large dimaond correctly

--- Diamond.tests/BehaviourTests.cs
+++ Diamond.tests/BehaviourTests.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace Diamond.tests
 {
-    internal class BehaviourTests
+    public class BehaviourTests
     {
+        [Fact]
+        public void Diamond_renders_correctly()
+        {
+            var expectation = new string[] {
+                                    "_____A_____",
+                                    "____B_B____",
+                                    "___C___C___",
+                                    "__D_____D__",
+                                    "_E_______E_",
+                                    "F_________F",
+                                    "_E_______E_",
+                                    "__D_____D__",
+                                    "___C___C___",
+                                    "____B_B____",
+                                    "_____A_____"};
+
+            var diamond = Diamond.Create('F');
+
+            Assert.Equal(expectation, diamond);
+        }
     }
 }
