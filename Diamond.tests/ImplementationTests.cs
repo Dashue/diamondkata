6a2840e0e90cba46abcc9bdd795279192342d69b
Johan Nilsson <johan@softyltd.co.uk>
2024-05-12 16:00:46 +0100

    Creates correct number of rows

--- Diamond.tests/ImplementationTests.cs
+++ Diamond.tests/ImplementationTests.cs
@@ -3,9 +3,11 @@ namespace Diamond.tests
     public class ImplementationTests
     {
         [Fact]
-        public void Test1()
+        public void Creates_correct_number_of_rows()
         {
-
+            Assert.Equal(1, Diamond.Create('A').Length);
+            Assert.Equal(3, Diamond.Create('B').Length);
+            Assert.Equal(5, Diamond.Create('C').Length);
         }
     }
 }
--- Diamond/CreateDiamond.cs
+++ Diamond/CreateDiamond.cs
@@ -1,7 +1,18 @@
-﻿namespace Diamond
+﻿
+namespace Diamond
 {
-    public class CreateDiamond
+    public static class Diamond
     {
+        private static List<char> _letters = Enumerable.Range(65, 26).Select(x => (char)x).ToList();
 
+        public static string[] Create(char maxLetter)
+        {
+            int index = _letters.IndexOf(maxLetter);
+            char[] previousLetters = _letters.GetRange(0, index).ToArray();
+
+            List<char> letters = new List<char>([..previousLetters, maxLetter, ..previousLetters.Reverse()]);
+
+            return letters.Select(x => "_").ToArray();
+        }
     }
 }
